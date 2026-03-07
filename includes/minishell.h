98807ab44b5df1082608bb2a30670d3f71a596db
Oery <coincoin@baozi>
2026-03-07 19:56:56 +0100

    init: add base structs

---
+++ includes/minishell.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 16:55:40 by Oery              #+#    #+#             */
+/*   Updated: 2026/03/07 19:50:41 by Oery             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// TODO: Fields
+// > History
+// > Last command exit code
+// > Signals?
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// TODO: Bonus operations
+typedef enum e_ops
+{
+	NONE,
+	INSERT_CMD,
+	INSERT_VAR,
+	REDIRECT_INPUT,
+	REDIRECT_OUTPUT,
+	READ_UNTIL,
+	REDIRECT_APPEND,
+	PIPE,
+}					t_ops;
+
+typedef struct s_node
+{
+	t_ops			op;
+	int				done;
+	char			*output;
+	struct s_node	*left;
+	struct s_node	*right;
+}					t_node;
+
+typedef struct s_ctx
+{
+	char			**env;
+}					t_ctx;
+
+#endif

dceb9ecf989d6b42824ea1d753842eceb73de421
Oery <coincoin@baozi>
2026-03-11 19:32:49 +0100

    tasks: mini_exit should return last exit code

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,11 +6,14 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 18:30:12 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/11 18:47:15 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/11 19:32:17 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// TODO: Exit Code of mini_exit should be
+// > the same as the one of the last executed command
 
 // TODO: Pressing Ctrl + D add exit to the current line and run it
 int	mini_exit(t_ctx *ctx)

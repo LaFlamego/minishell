6f4a7704258670238ccc151bca24dc4309e238df
Oery <coincoin@baozi>
2026-03-13 00:12:40 +0100

    wip: update prompt testing logic

--- src/prompt_display.c
+++ src/prompt_display.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 13:15:33 by crevette          #+#    #+#             */
-/*   Updated: 2026/03/12 21:23:08 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/12 23:59:55 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 
+// TODO: Handle new lines
+
 // FIXME: Ctrl+D should print exit
 // > on the current command line
 // > not the next one.
 void	prompt_display(t_ctx *ctx)
 {
-	char	*input;
-	char	**argv;
-	int		argc;
+	char			*input;
+	char			**argv;
+	int				argc;
+	unsigned int	exit_code;
 
+	exit_code = 0;
 	while (!ctx->exit)
 	{
 		input = readline("(=^.^=)$ ");
@@ -43,9 +47,17 @@ void	prompt_display(t_ctx *ctx)
 			mini_unset(argc, argv, ctx);
 		else if (ft_streq(argv[0], "export"))
 			mini_export(argc, argv, ctx);
+		else if (ft_streq(argv[0], "env"))
+			mini_env(argc, argv, ctx);
 		else if (ft_streq(argv[0], "exit"))
-			mini_exit(argc, argv, ctx);
+		{
+			exit_code = mini_exit(argc, argv, ctx);
+		}
+		printf("exit code: %u\n", exit_code);
+		// TODO: Check if input is good
+		add_history(input);
 		free_splits(argv);
 		free(input);
 	}
+	rl_clear_history();
 }

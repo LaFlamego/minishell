d7beb7331dc9a1f2bb2a827b9e3b5040d432f5ae
Oery <coincoin@baozi>
2026-03-12 23:45:06 +0100

    feat: handle mini_exit arguments

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,42 +6,56 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 18:30:12 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/12 21:24:03 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/12 23:41:04 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "minishell.h"
+#include <limits.h>
+
+#define BASE 10
 
 // TODO: Default Exit Code of mini_exit should be
 // > the same as the one of the last executed command
 
-// NOTE: Argument is a byte
-// > -1 should return 255
-
-int	parse_arg(char *arg, unsigned int *exit_code)
+static int	will_overflow(char c, long long n, long long sign)
 {
-	size_t	i;
+	if (sign > 0)
+	{
+		if (n > LLONG_MAX / BASE || (n * BASE > LLONG_MAX - (c - '0')))
+			return (1);
+	}
+	else
+	{
+		if (n < LLONG_MIN / BASE || (n * BASE) < LLONG_MIN + (c - '0'))
+			return (1);
+	}
+	return (0);
+}
+
+static int	parse_arg(char *arg, long long *exit_code)
+{
+	size_t		i;
+	long long	sign;
 
 	*exit_code = 0;
 	i = 0;
-	if (arg[i] == '-')
+	sign = 1;
+	if (arg[i] == '-' || arg[i] == '+')
 	{
+		if (arg[i] == '-')
+			sign = -1;
 		i++;
 	}
-	else if (arg[i] == '+')
-	{
-		if (!arg[i + 1])
-			return (1);
-		i++;
-	}
-	// TODO: Handle -/+ sign
+	if (!arg[i])
+		return (1);
 	while (arg[i])
 	{
-		if (!ft_isdigit(arg[i]))
+		if (!ft_isdigit(arg[i]) || will_overflow(arg[i], *exit_code, sign))
 			return (1);
-		*exit_code *= 10;
-		*exit_code += arg[i] - '0';
+		*exit_code *= BASE;
+		*exit_code += sign * (arg[i] - '0');
 		i++;
 	}
 	return (0);
@@ -49,23 +63,24 @@ int	parse_arg(char *arg, unsigned int *exit_code)
 
 unsigned int	mini_exit(int argc, char *argv[], t_ctx *ctx)
 {
-	unsigned int	exit_code;
+	long long	exit_code;
 
 	ft_printf("exit\n");
 	if (argc > 2)
 	{
-		ft_dprintf(2, "exit: too many arguments\n");
+		ft_dprintf(2, "minishell: exit: too many arguments\n");
 		return (2);
 	}
 	if (argc > 1)
 	{
 		if (parse_arg(argv[1], &exit_code))
 		{
-			ft_dprintf(2, "exit: %s: numeric argument required\n", argv[1]);
+			ft_dprintf(2, "minishell: exit: %s: numeric argument required\n",
+				argv[1]);
 			return (2);
 		}
 		ctx->exit = true;
-		return (exit_code);
+		return ((unsigned int)(unsigned char)exit_code);
 	}
 	ctx->exit = true;
 	return (0);

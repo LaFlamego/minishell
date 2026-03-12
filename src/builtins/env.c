7263760df0ff47e37f884cf4bc9b909bc00f3cfc
crevette <coincoin@baozi>
2026-03-12 16:17:46 +0100

    builtins: mini_env main part

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,41 +6,19 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 16:14:45 by crevette          #+#    #+#             */
-/*   Updated: 2026/03/12 14:57:29 by crevette         ###   ########.fr       */
+/*   Updated: 2026/03/12 16:16:24 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // *DONE env 
-// TODO env NAME=val NAME=val ... NAME=val
-// TODO env NAME=val NAME=val command(with multiple args it should have)
+// *DONE env NAME=val NAME=val ... NAME=val
+// *DONE env NAME=val NAME=val command(with multiple args it should have)
 // TODO error msg - use perror
 
 #include "minishell.h"
 #include <stdbool.h>
 
-void	store_keypair(t_ctx *ctx, char *var);
 void	env_vars_list(t_ctx *ctx);
-
-t_env *local;
-
-local = ft_env_new();
-
-size_t i;
-char  *key;
-char  *value;
-
-i = 0;
-while (i < ctx->env->size - 1)
-{
-  key = ctx->env->data[i];
-  if (!ft_env_find(local, key))
-  {
-    value = ft_strchr(key, '=');
-    if (value)
-      ft_env_set(local, key, value);
-  }
-  i++;
-}
 
 static bool all_vars(char **argv)
 {
@@ -53,7 +31,7 @@ static bool all_vars(char **argv)
 	return (true);
 }
 
-static	bool existed_in_env(char *var, t_ctx *ctx)
+void	get_path(char **cmds)
 {
 
 }
@@ -66,25 +44,26 @@ static void	run_cmd(char **cmds, t_ctx *ctx)
 void	mini_env(int argc, char **argv, t_ctx *ctx)
 {
 	size_t	i;
+	t_env	*local;
 
-	i = 0;
+	local = ft_env_new();
+	if (!local)
+		return ;
 	if (argc == 1)
 		vars_list(ctx);
 	else if (argc > 1)
 	{
+		i = 1;
 		if (all_vars)
 			vars_list(ctx);
-		++i;
 		while (argv[i] && ft_strchr(argv[i], '='))
 		{
-			if (!existed_in_env(argv[i], ctx))
-			{
-				store_keypair(ctx, argv[i]);
-				if (all_vars)
-					printf("%s\n", argv[i]);
-			}
+			ft_env_set(local, argv[i]);
+			if (all_vars)
+				printf("%s\n", argv[i]);
 			++i;
 		}
+		ft_env_merge(local, ctx->env);
 		if (argv[i])
 			run_cmd((argv + i), ctx);
 	}

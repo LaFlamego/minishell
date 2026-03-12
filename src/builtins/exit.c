bcc46046426de91bbe97963a753be23d1f63988e
Oery <coincoin@baozi>
2026-03-12 15:21:32 +0100

    tasks: exit can return a custom exit code

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 18:30:12 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/11 19:32:17 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/12 15:20:49 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 // TODO: Exit Code of mini_exit should be
 // > the same as the one of the last executed command
+// > N if mini_exit has 1 argument
 
-// TODO: Pressing Ctrl + D add exit to the current line and run it
 int	mini_exit(t_ctx *ctx)
 {
 	ctx->exit = true;

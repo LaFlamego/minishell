a500c262397f93cf8b1b115415ae08f480ad0767
Oery <coincoin@baozi>
2026-03-12 19:17:10 +0100

    feat: skip invalid arguments

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,11 +6,17 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 23:56:01 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/11 18:30:52 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/12 19:16:18 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdbool.h>
+
+static bool	is_valid(const char *key)
+{
+	return (ft_strchr(key, '=') == NULL);
+}
 
 int	mini_unset(int argc, char **argv, t_ctx *ctx)
 {
@@ -19,7 +25,8 @@ int	mini_unset(int argc, char **argv, t_ctx *ctx)
 	i = 1;
 	while (i < argc)
 	{
-		ft_env_unset(ctx->env, argv[i]);
+		if (is_valid(argv[i]))
+			ft_env_unset(ctx->env, argv[i]);
 		i++;
 	}
 	return (0);

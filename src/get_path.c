b323df0e5c7ef7a25c53a485e0f72ddef55b3a5c
crevette <coincoin@baozi>
2026-03-12 21:12:51 +0100

    add: get_path to get cmd path

---
+++ src/get_path.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/24 13:59:50 by crevette          #+#    #+#             */
+/*   Updated: 2026/03/12 20:12:13 by crevette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <unistd.h>
+
+static char	**extract_path(char **envp)
+{
+	char	*path_str;
+	char	**res;
+
+	if (!envp || !*envp)
+		return (NULL);
+	path_str = NULL;
+	while (*envp)
+	{
+		if (ft_strncmp(*envp, "PATH=", 5) == 0)
+		{
+			path_str = *envp + 5;
+			break ;
+		}
+		++envp;
+	}
+	if (!path_str)
+		return (NULL);
+	res = ft_split(path_str, ':');
+	return (res);
+}
+
+static bool	cmd_access(bool f_no_x, char *target_path)
+{
+	if (access(target_path, F_OK) == 0)
+	{
+		if (access(target_path, X_OK) == 0)
+			return (true);
+		else
+			f_no_x = true;
+		// TODO return for permission issue and free
+	}
+	return (false);
+}
+
+static char	*find_path(bool f_no_x, char *cmd_name, char **cddt_paths)
+{
+	char	*tpr;
+	char	*res;
+
+	if (cddt_paths)
+	{
+		while (*cddt_paths)
+		{
+			tpr = ft_strjoin(*cddt_paths, "/");
+			if (!tpr)
+				return (NULL);
+			res = ft_strjoin(tpr, cmd_name);
+			free(tpr);
+			if (!res)
+				return (NULL);
+			if (cmd_access(f_no_x, res))
+				return (res);
+			free(res);
+			++cddt_paths;
+		}
+	}
+	return (NULL);
+}
+
+char	*get_path(char *cmd_name, t_env *env)
+{
+	char	*path;
+	char	*path_find;
+	char	**cddt_paths;
+
+	if (!cmd_name)
+		return (NULL);
+	// cmd->is_abs = false;
+	path = ft_strchr(cmd_name, '/');
+	cddt_paths = extract_path(env->data);
+	path_find = find_path(false, cmd_name, cddt_paths);
+	if (cddt_paths)
+		free_splits(cddt_paths);
+	if (path)
+	{
+		path = cmd_name;
+		//cmd->is_abs = true;
+		if (access(path, X_OK) == 0 && access(path, F_OK) == 0)
+			return (path);
+	}
+	else if (path_find)
+		return (path_find);
+	return (NULL);
+}

ec4e1281ef076d8d608ff6096a9b53d2feb6ffff
Oery <coincoin@baozi>
2026-03-13 17:17:47 +0100

    libft: misc changes

--- lib/libft/src/string/ft_string.c
+++ lib/libft/src/string/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 17:00:54 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/13 00:56:20 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/13 16:25:40 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 char	*ft_string_alloc(t_string *s, size_t size)
 {
-	s->length = 0;
+	s->size = 0;
 	s->content = malloc(size);
 	if (s->content == NULL)
 	{
@@ -46,8 +46,39 @@ t_string	ft_string_from_cstring(char *str)
 {
 	t_string	s;
 
-	s.length = ft_strlen(str);
+	s.size = ft_strlen(str);
 	s.capacity = 0;
 	s.content = str;
 	return (s);
+}
+
+void	*ft_string_resize(t_string *s, size_t size)
+{
+	void	*data;
+
+	data = malloc(size);
+	if (!data)
+		return (NULL);
+	if (s->content)
+	{
+		ft_memmove(data, s->content, s->size);
+		free(s->content);
+	}
+	s->content = data;
+	s->capacity = size;
+	return (s->content);
+}
+
+size_t	ft_string_push(t_string *s, char c)
+{
+	if (s->size + 1 > s->capacity)
+	{
+		if (s->capacity == 0)
+			s->capacity = 1;
+		if (!ft_string_resize(s, s->capacity * 2))
+			return (0);
+	}
+	s->content[s->size] = c;
+	s->size++;
+	return (s->size);
 }
--- lib/libft/src/string/ft_string.h
+++ lib/libft/src/string/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 00:53:09 by Oery              #+#    #+#             */
-/*   Updated: 2026/03/13 00:55:46 by Oery             ###   ########.fr       */
+/*   Updated: 2026/03/13 16:26:50 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@
 typedef struct s_string
 {
 	char	*content;
-	size_t	length;
+	size_t	size;
 	size_t	capacity;
 }			t_string;
 
 char		*ft_string_alloc(t_string *s, size_t size);
 char		*ft_string_realloc(t_string *s, size_t new_size);
 char		*ft_string_free(t_string *s);
+void		*ft_string_resize(t_string *s, size_t size);
+size_t		ft_string_push(t_string *s, char c);
 t_string	ft_string_from_cstring(char *str);
 
 #endif

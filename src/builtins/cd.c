d34ec1de1f23454dfb5d5967337a400b78e09427
crevette <coincoin@baozi>
2026-03-16 19:05:04 +0100

    builtins: done cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,28 +6,66 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 12:18:23 by crevette          #+#    #+#             */
-/*   Updated: 2026/03/09 20:56:13 by crevette         ###   ########.fr       */
+/*   Updated: 2026/03/16 18:05:25 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "libft/string.h"
+#include "libft/printf.h"
+#include "libft.h"
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 
-void	mini_cd(int argc, char **argv)
+#define BUFFER_SIZE 64
+
+unsigned int update_cwd(char *key, t_ctx *ctx)
 {
-	size_t	size;	
+	t_string buf;
+	int key_len;
+
+	buf = ft_string_from_cstring_alloc(key);
+	if (!buf.content)
+		return (1);
+	key_len = ft_strlen(key);
+	while (getcwd(buf.content + key_len, buf.capacity - key_len) == NULL && errno == ERANGE)
+	{
+		if (!ft_string_realloc(&buf, buf.capacity * 2))
+			return (free(buf.content), 1);
+	}
+	if (errno && !buf.content)
+		return (free(buf.content), 1);
+	else
+	{
+		ft_env_set(ctx->env, buf.content);
+		free(buf.content);
+	}
+	return (0);
+}
+
+unsigned int mini_cd(int argc, char **argv, t_ctx *ctx)
+{
 
 	if (argc > 2)
-		ft_fprintf("cd: too many arguments\n");
-	else if (argv[1] && chdir(argv[1]) == -1)
-		perror("cd");
-	while (!getcwd(buf, size))
+		ft_printf("minishell: cd: too many arguments\n");
+	else if (argc < 2)
+		ft_printf("minishell: cd: too few arguments\n");
+	if (argc == 2)
 	{
-		if (errno == ERANGE)
-		{	
-			size *= 2;
-			buf = (char *)malloc(sizeof(char) * size);
-
+		if (argv[1] && argv[1][0])
+		{
+			if (update_cwd("OLDPWD=", ctx) == 1)
+				return (1);
 		}
-	} 
+		if (argv[1] && chdir(argv[1]) == -1)
+			return (perror("cd"), 1);
+		else
+		{
+			if (update_cwd("PWD=", ctx) == 1)
+				return (1);
+		}
+		return (0);
+	}
+	return (1);
 }

a1519da7d5e52379f2aacb5a9c34b634dfbda361
Oery <coincoin@baozi>
2026-04-08 17:46:37 +0200

    fix: token_scan_string()

--- src/scanner/token/token_scan_string.c
+++ src/scanner/token/token_scan_string.c
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 15:39:47 by Oery              #+#    #+#             */
-/*   Updated: 2026/04/07 15:46:20 by Oery             ###   ########.fr       */
+/*   Updated: 2026/04/08 17:13:33 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,13 @@ t_token	*token_scan_string(t_scanner *s)
 {
 	char	*text;
 
-	while (scanner_peek(s) != '\'' && is_at_end(s))
+	while (scanner_peek(s) != '\'' && scanner_peek(s) != '\0')
 	{
 		scanner_advance(s);
 	}
-	if (is_at_end())
-	{
-		scanner_error(s);
-		// TODO: Display Error
-		return (NULL);
-	}
+	if (scanner_peek(s) == '\0')
+		return (scanner_error(s, '\'', s->current));
 	scanner_advance(s);
 	text = ft_substr(s->source, s->start + 1, s->current - 1);
-	scanner_add_token_lit(s, STRING, text);
+	return (scanner_add_token_lit(s, STRING, text));
 }

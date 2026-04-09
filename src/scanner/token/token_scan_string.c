070895bfd78ec0300ebcb94f2b3bf023ad7fd3bc
Oery <coincoin@baozi>
2026-04-09 16:31:27 +0200

    fix: invalid string scanning

--- src/scanner/token/token_scan_string.c
+++ src/scanner/token/token_scan_string.c
@@ -6,26 +6,26 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 15:39:47 by Oery              #+#    #+#             */
-/*   Updated: 2026/04/08 17:13:33 by Oery             ###   ########.fr       */
+/*   Updated: 2026/04/09 16:24:36 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../scanner.h"
 #include "./token.h"
 
-// TODO: check length calculation
-// > start+1 and current-1 -> content inside the quotes
 t_token	*token_scan_string(t_scanner *s)
 {
 	char	*text;
 
-	while (scanner_peek(s) != '\'' && scanner_peek(s) != '\0')
+	while (scanner_peek(s) && scanner_peek(s) != '\'')
 	{
 		scanner_advance(s);
 	}
 	if (scanner_peek(s) == '\0')
+	{
 		return (scanner_error(s, '\'', s->current));
+	}
 	scanner_advance(s);
-	text = ft_substr(s->source, s->start + 1, s->current - 1);
+	text = ft_substr(s->source, s->start + 1, s->current - s->start - 2);
 	return (scanner_add_token_lit(s, STRING, text));
 }

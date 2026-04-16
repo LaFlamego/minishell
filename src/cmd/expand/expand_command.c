48b026422a60482ecf721bddb45ed4e7d8649f69
Oery <coincoin@baozi>
2026-04-16 12:36:31 +0200

    wip: command expansion with new format

--- src/cmd/expand/expand_command.c
+++ src/cmd/expand/expand_command.c
@@ -5,55 +5,65 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/22 20:29:42 by Oery              #+#    #+#             */
-/*   Updated: 2026/04/02 01:24:40 by Oery             ###   ########.fr       */
+/*   Created: 2026/04/15 17:39:11 by Oery              #+#    #+#             */
+/*   Updated: 2026/04/15 18:01:52 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./expand.h"
 #include "libft.h"
-#include "src/env/env.h"
+#include "src/scanner/token/token.h"
+#include <stdlib.h>
 
-static bool	is_sq_string(t_string *s)
+// WARN: Watch out for errors
+
+void	expand_word(t_array *cmd, t_array *tokens)
 {
-	return (s->content[0] == '\'');
+	size_t	i;
+	t_token	*t;
+
+	i = 0;
+	while (i < tokens->size)
+	{
+		t = tokens->data[i];
+		if (t->type == DOLLAR)
+		{
+			// ident is at index i + 1
+		}
+		if (t->type == STRING)
+		{
+			// ft_strdup()
+		}
+		i++;
+	}
+	// TODO: Token is either a DOLLAR or a STRING
+	// DOLLAR must be followed by STRING
 }
 
-static bool	is_string(t_string *s)
+void	expand_words(t_array *cmd, t_array *words)
 {
-	if (s->content[0] == '"')
-		return (true);
-	if (s->content[0] == '\'')
-		return (true);
-	return (false);
-}
+	size_t	i;
 
-// TODO: Expand substitutions
-// > does nested substitutions exist or are they done at the same time?
-//
-// TODO: Handle errors
-// > How to deal with syntax errors
-t_array	*cmd_expand_command(t_env *env, t_array *words)
-{
-	size_t		i;
-	t_string	*word;
-	t_array		*expanded;
-
-	expanded = ft_array_new();
-	if (!expanded)
-		return (NULL);
 	i = 0;
 	while (i < words->size)
 	{
-		word = words->data[i];
-		if (!is_sq_string(word))
-			word = cmd_expand_word(env, word);
-		if (!is_string(word) && ft_strchr(word->content, '*'))
-			cmd_expand_wildcard(env, words);
-		else
-			ft_array_push(expanded, word);
+		// TODO: handle error
+		expand_word(cmd, words->data[i]);
 		i++;
 	}
-	i = 0;
-	return (expanded);
+}
+
+// TODO: Turn a stream of token into argvs
+char	**expand_cmd(t_array *words)
+{
+	t_array	*cmd;
+	char	**ret;
+
+	cmd = ft_array_new();
+	if (!cmd)
+		return (NULL);
+	expand_words(cmd, word);
+	ret = (char **)cmd->data;
+	free(cmd);
+	ft_strjoin(cmd->data, '0');
+	return (ret);
 }

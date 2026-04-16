69e5fe8b0155435eae85ab4dcd2c8105b1381df2
Oery <coincoin@baozi>
2026-04-16 12:43:45 +0200

    git: merge diverging repos

--- src/cmd/cmd.h
+++ src/cmd/cmd.h
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 16:50:13 by Oery              #+#    #+#             */
-/*   Updated: 2026/04/16 10:17:36 by crevette         ###   ########.fr       */
+/*   Updated: 2026/04/16 12:42:48 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,52 +16,18 @@
 # include "libft.h"
 # include "src/ctx/ctx.h"
 
-// TODO: Add bonuses
-// typedef enum s_kind
-// {
-// 	STRING,
-// 	INSERT_CMD,
-// 	INSERT_ENV_VAR,
-// 	REDIRECT_INPUT,
-// 	REDIRECT_OUTPUT,
-// 	READ_UNTIL,
-// 	REDIRECT_APPEND,
-// 	PIPE,
-// }				t_kind;
-
-enum				e_kind
-{
-	COMMAND,
-};
-
-typedef struct s_op
-{
-	struct s_node	*left;
-	struct s_node	*right;
-}					t_op;
-
-typedef struct s_node
-{
-	enum e_kind		kind;
-	union
-	{
-		t_op		op;
-		t_array		*tokens;
-	};
-}					t_node;
-
 typedef struct s_cmd
 {
-	char			*path;
-	char			**cmds;
-	//int				nb;
-	bool			valid;
-	bool			f_no_x;
-	bool			is_path;
-}					t_cmd;
+	char		*path;
+	char		**cmds;
+	// int				nb;
+	bool		valid;
+	bool		f_no_x;
+	bool		is_path;
+}				t_cmd;
 
-bool				is_sq_string(t_string *s);
-unsigned int		cmd_handle(const char *input, t_ctx *ctx);
-void				cmd_quotes_remove(const t_array *words);
+bool			is_sq_string(t_string *s);
+unsigned int	cmd_handle(const char *input, t_ctx *ctx);
+void			cmd_quotes_remove(const t_array *words);
 
 #endif

3caf47dab1ec5e351627bdcd2f6903171749ad82
crevette <coincoin@baozi>
2026-04-24 13:04:51 +0200

    add: track tree nodes

--- src/cmd/exec/exec_track.c
+++ src/cmd/exec/exec_track.c
@@ -6,38 +6,28 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 20:24:51 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/22 19:53:15 by crevette         ###   ########.fr       */
+/*   Updated: 2026/04/23 23:46:23 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../cmd.h"
 # include "../tree/node.h"
 
-void    track_nodes(t_cmd_node *node)
-{
-	t_bin_op_args	*ops;
+//TODO < > << >>
 
+unsigned int	track_nodes(t_cmd_node *node)
+{
 	if (!node)
-		return ;
-	ops = node->data;
+		return(1);
 	if (node->kind == OP_AND || node->kind == OP_OR)
 	{
 		if (node->kind == OP_AND)
-		{
-
-		}
+			handle_and_sign(node);
 		if (node->kind == OP_OR)
-		{
-			
-		}
-		track_nodes(ops->left);
+			handle_or_sign(node);
 	}
 	if (node->kind == PIPELINE)
-	{
-
-	}
+		handle_pipe_sign(node);
 	if (node->kind == COMMAND)
-	{
-
-	}
+		handle_single_command(node);
 }

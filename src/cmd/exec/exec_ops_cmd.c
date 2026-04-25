c7fca0eef7e5bf488341d0cbfd577c1fd9f403e4
crevette <coincoin@baozi>
2026-04-25 17:15:50 +0200

    ref: call exec_cmd in ops cmd

--- src/cmd/exec/exec_ops_cmd.c
+++ src/cmd/exec/exec_ops_cmd.c
@@ -6,23 +6,24 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 15:08:52 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/25 16:41:37 by crevette         ###   ########.fr       */
+/*   Updated: 2026/04/25 17:14:00 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "cmd.h"
 #include "../tree/node.h"
+#include "src/ctx/ctx.h"
 
-unsigned int	handle_single_command(t_cmd_node *node)
+unsigned int	handle_single_command(t_cmd_node *node, t_ctx *ctx)
 {
 	t_array			*cmd_node;
 	unsigned int	exit_code;
-	char			**args;
+	char			**argv;
 
 	cmd_node = node->data;
-	args = expand_args(cmd_node->data);
-	if (!args)
+	argv = expand_args(cmd_node->data);
+	if (!argv)
 		return (1);
-	return (cmd_exec());
+	return (cmd_exec(ctx, argv));
 }

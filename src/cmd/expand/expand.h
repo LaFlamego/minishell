b202a5aeb8cab9c956cb7b4db78538475c995613
Oery <coincoin@baozi>
2026-04-25 16:03:52 +0200

    feat: add expansion functions signatures

--- src/cmd/expand/expand.h
+++ src/cmd/expand/expand.h
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 20:21:43 by Oery              #+#    #+#             */
-/*   Updated: 2026/04/25 15:37:44 by Oery             ###   ########.fr       */
+/*   Updated: 2026/04/25 15:59:04 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ t_string	*cmd_expand_word(t_env *env, t_string *word);
 void		*cmd_expand_wildcard(t_env *env, t_array *words);
 
 size_t		get_arg_expanded_size(t_array *tokens, t_env *env);
+
+char		**expand_args(t_array *args);
+char		*expand_redirection(t_array *tokens);
 
 #endif

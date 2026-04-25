bdcbecf414dd5f4008348251e000e79a9c5b798a
crevette <coincoin@baozi>
2026-04-25 16:10:26 +0200

    add: single cmd

--- src/cmd/exec/exec_ops_cmd.c
+++ src/cmd/exec/exec_ops_cmd.c
@@ -6,14 +6,18 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 15:08:52 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/25 15:09:27 by crevette         ###   ########.fr       */
+/*   Updated: 2026/04/25 15:40:11 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "cmd.h"
 #include "../tree/node.h" 
 
-void	handle_single_command()
+unsigned int	handle_single_command(t_cmd_node *node)
 {
+	t_array		*cmd_node;
+	unsigned int	exit_code;
 
+	cmd_node = node->data;
 }

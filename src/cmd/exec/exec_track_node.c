3533ac141f35ee2ca986ee4fb34918245513b32f
crevette <coincoin@baozi>
2026-04-30 18:59:03 +0200

    fix: remove is_pipe flag in track_node

--- src/cmd/exec/exec_track_node.c
+++ src/cmd/exec/exec_track_node.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 20:24:51 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/29 23:04:24 by crevette         ###   ########.fr       */
+/*   Updated: 2026/04/30 18:58:38 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,9 @@ unsigned int	track_node(t_cmd_node *node, t_ctx *ctx)
 	if (node->kind == COMMAND)
 		handle_single_command(node, ctx, &exec_ctx);
 	if (node->kind == PIPELINE)
-	{
-		exec_ctx.is_pipe_cmd = true;
 		handle_pipe_sign(node, &exec_ctx);
-	}
-	else
-	{
-		exec_ctx.is_pipe_cmd = false;
-		if (node->kind == OP_AND)
-			handle_and_sign(node);
-		if (node->kind == OP_OR)
-			handle_or_sign(node);
-	}
+	if (node->kind == OP_AND)
+		handle_and_sign(node);
+	if (node->kind == OP_OR)
+		handle_or_sign(node);
 }

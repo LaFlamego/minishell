aea2011ac9a276b7e020637d01ec1f3f8a5236eb
crevette <coincoin@baozi>
2026-05-03 20:22:54 +0200

    add: init exec_ctx in track_node and add todo

--- src/cmd/exec/exec_track_node.c
+++ src/cmd/exec/exec_track_node.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 20:24:51 by crevette          #+#    #+#             */
-/*   Updated: 2026/05/02 00:27:20 by crevette         ###   ########.fr       */
+/*   Updated: 2026/05/03 20:22:18 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 #include "src/ctx/ctx.h"
 #include <unistd.h>
 
-//TODO (DONE)< > << >>
-//TODO (DONE)pass fd to proceed
-//TODO check out free exit and free cmd path there
-//TODO go through the whole execution preocess
+//TODO (X)< > << >>
+//TODO (X)pass fd to proceed
+//TODO (X)check out free exit and free cmd path there
+//TODO (X)go through the whole execution process
 unsigned int	track_node(t_cmd_node *node, t_ctx *ctx)
 {
 	t_exec_ctx	exec_ctx;
 
-	// if (!node)
-	// 	return(1);
+	init_exec_ctx(exec_ctx);
 	if (node->kind == COMMAND)
 		handle_single_command(node, ctx, &exec_ctx);
 	if (node->kind == PIPELINE)

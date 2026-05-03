c61de16cd3257afa3ef35e523548c7bf996ad5fa
crevette <coincoin@baozi>
2026-05-03 16:55:06 +0200

    ref: modify init_exec_ctx

--- src/cmd/exec/exec_init.c
+++ src/cmd/exec/exec_init.c
@@ -6,18 +6,19 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 10:13:53 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/16 13:30:49 by crevette         ###   ########.fr       */
+/*   Updated: 2026/05/03 16:54:34 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cmd.h"
 #include "libft.h"
 
-void    init_cmd(t_cmd *cmd)
+void    init_exec_ctx(t_exec_ctx *exec)
 {
-    ft_bzero(cmd, sizeof(t_cmd));
-    cmd->path = NULL;
-    cmd->cmds = NULL;
-    cmd->valid = false;
-    cmd->f_no_x = false;
+    ft_bzero(exec, sizeof(t_exec_ctx));
+    exec->cmd.path = NULL;
+	exec->fd.in = -1;
+	exec->fd.out = -1;
+	exec->pipe.fd = -1;
+	exec->pipe.index = 0;
 }

a57cdd734a48563ec3cc58f4b150a61299683078
Oery <coincoin@baozi>
2026-05-07 22:40:22 +0200

    ref: replace gnl with readline

--- src/cmd/expand/expand_redirections.c
+++ src/cmd/expand/expand_redirections.c
@@ -6,16 +6,21 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/26 22:01:11 by crevette          #+#    #+#             */
-/*   Updated: 2026/05/05 14:40:08 by crevette         ###   ########.fr       */
+/*   Updated: 2026/05/07 22:33:22 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../exec/exec.h"
-#include <fcntl.h>
-#include <unistd.h>
 #include "libft.h"
+#include "src/utils/utils.h"
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+// TODO: How to handle errors here?
 
 void	redirect_in(char *file_to, t_exec_ctx *exec_ctx)
 {
@@ -45,6 +50,7 @@ void	redirect_out(char *file_to, t_exec_ctx *exec_ctx)
 	exec_ctx->redir = WRITE_OUT;
 }
 
+// TODO: Maybe add a history line ehre
 void	redirect_in_until(t_exec_ctx *exec_ctx, char *limiter)
 {
 	char	*new_line;
@@ -54,19 +60,17 @@ void	redirect_in_until(t_exec_ctx *exec_ctx, char *limiter)
 
 	len_limiter = ft_strlen(limiter);
 	pipe_build(&fd_in, &fd_out);
-	new_line = get_next_line(0);
+	new_line = readline("> ");
 	while (new_line != NULL)
 	{
 		if (len_limiter > 0 && ft_strncmp(new_line, limiter, len_limiter) == 0)
-		{
-			free(new_line);
 			break ;
-		}
-		else
-			write(fd_out, new_line, ft_strlen(new_line));
+		write(fd_out, new_line, ft_strlen(new_line));
+		write(fd_out, "\n", 1);
 		free(new_line);
-		new_line = get_next_line(0);
+		new_line = readline("> ");
 	}
+	free(new_line);
 	fd_close_reset(NULL, &fd_out, NULL);
 	exec_ctx->fd.in = fd_in;
 	exec_ctx->redir = HEREDOC;
@@ -85,4 +89,3 @@ void	redirect_out_append(char *file_to, t_exec_ctx *exec_ctx)
 	exec_ctx->fd.out = fd_file_to;
 	exec_ctx->redir = APPEND;
 }
-

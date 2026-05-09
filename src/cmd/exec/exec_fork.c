08af4b295541c11e43ce6528775aaa57349778ac
crevette <coincoin@baozi>
2026-05-09 15:36:57 +0200

    ref: remove free in children

--- src/cmd/exec/exec_fork.c
+++ src/cmd/exec/exec_fork.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 21:35:14 by Oery              #+#    #+#             */
-/*   Updated: 2026/05/08 19:33:16 by crevette         ###   ########.fr       */
+/*   Updated: 2026/05/09 15:03:38 by crevette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,23 @@
 // > execve: Permission denied
 // > bash: /home/oery/Documents/42/minishell: Is a directory
 
-static void	get_and_exec_cmd(char *argv[], t_exec_ctx *exec, t_env *env)
+static unsigned int	get_and_exec_cmd(char *argv[], t_exec_ctx *exec, t_env *env)
 {
-	char	*cmd_path;
+	char			*cmd_path;
+	unsigned int	exit_code;
 
-	cmd_exec_get_path(argv[0], exec, env);
-	cmd_path = exec->cmd.path;
-	if (!cmd_path)
+	exit_code = cmd_exec_get_path(argv[0], exec, env);
+	if (exit_code != 0)
 	{
-		//ft_dprintf(2, "minishell: '%s': %s\n", argv[0], strerror(errno));
-		exit(errno);
+		free_cmd_path(exec);
+		exit(exit_code);
 	}
+	cmd_path = exec->cmd.path;
+	// if (!cmd_path)
+	// {
+	// 	ft_dprintf(2, "minishell: '%s': %s\n", argv[0], strerror(errno));
+	// 	exit(errno);
+	// }
 	execve(cmd_path, argv, (char **)env->data);
 	perror("execve");
 	exit(errno);
@@ -49,9 +55,8 @@ pid_t	cmd_exec_fork(char *argv[], t_exec_ctx *exec, t_env *env)
 	{
 		redir_fd(exec, false);
 		get_and_exec_cmd(argv, exec, env);
-		free_cmd_path(exec);
 	}
-	else
-		(void)pid;
+	else if (pid > 0)
+		free_cmd_path(exec);
 	return (pid);
 }

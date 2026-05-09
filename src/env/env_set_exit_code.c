a4de7d05d846a91af3fca60a4854544ac4e71d8e
Oery <coincoin@baozi>
2026-05-09 15:46:50 +0200

    fix: exit code var not updating

--- src/env/env_set_exit_code.c
+++ src/env/env_set_exit_code.c
@@ -6,7 +6,7 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 12:55:09 by crevette          #+#    #+#             */
-/*   Updated: 2026/04/02 01:19:08 by Oery             ###   ########.fr       */
+/*   Updated: 2026/05/09 15:45:14 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,15 @@ int	env_set_exit_code(unsigned int exit_code, t_env *env)
 	t_string	var;
 	char		*val;
 
-	if (exit_code)
-	{
-		var = ft_string_from_cstring_alloc("?=");
-		if (!var.content)
-			return (1);
-		val = ft_itoa(exit_code);
-		if (!val)
-			return (1);
-		ft_string_push_str(&var, val);
-		free(val);
-		env_set(env, var.content);
-		free(var.content);
-	}
+	var = ft_string_from_cstring_alloc("?=");
+	if (!var.content)
+		return (1);
+	val = ft_itoa(exit_code);
+	if (!val)
+		return (1);
+	ft_string_push_str(&var, val);
+	free(val);
+	env_set(env, var.content);
+	free(var.content);
 	return (0);
 }

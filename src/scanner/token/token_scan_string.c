f2e5855d432b3a5e775e246d28e6eb504ce898c6
Oery <coincoin@baozi>
2026-05-10 22:12:13 +0200

    fix: double quoted empty string silently failing

--- src/scanner/token/token_scan_string.c
+++ src/scanner/token/token_scan_string.c
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 15:39:47 by Oery              #+#    #+#             */
-/*   Updated: 2026/05/05 00:59:43 by Oery             ###   ########.fr       */
+/*   Updated: 2026/05/10 22:10:01 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ t_token	*token_scan_string(t_scanner *s)
 // If string starts with a symbol or a number
 // It's length is 1
 // Else read until blank / symbol
+
+// FIXME: An empty string should not add any token
 
 static t_token	*scan_dollar(t_scanner *s)
 {
@@ -86,7 +88,7 @@ t_token	*token_scan_string_double(t_scanner *s)
 	}
 	if (!scanner_match(s, '"'))
 		return (scanner_error('"'));
-	if (s->current > s->start + 1)
+	if (s->current > s->start)
 	{
 		text = ft_substr(s->source, s->start, s->current - s->start - 1);
 		if (!text)
@@ -95,67 +97,3 @@ t_token	*token_scan_string_double(t_scanner *s)
 	}
 	return (t);
 }
-
-// // FIXME: This should not be responsible for parsing the variable
-// static t_token	*scan_part(t_scanner *s)
-// {
-// 	char	c;
-// 	char	*ident;
-//
-// 	c = scanner_peek(s);
-// 	while (c && c != '"')
-// 	{
-// 		if (c == '$')
-// 		{
-// 			// TODO: check this return value
-// 			token_scan_ident(s);
-// 		}
-// 		else
-// 		{
-// 			scanner_advance(s);
-// 			c = scanner_peek(s);
-// 		}
-// 	}
-// 	ident = ft_substr(s->source, s->start, s->current - s->start);
-// 	if (!ident)
-// 		return (NULL);
-// 	if (ft_streq(ident, "$"))
-// 	{
-// 		free(ident);
-// 		return (scanner_add_token(s, DOLLAR));
-// 	}
-// 	return (scanner_add_token_lit(s, STRING, ident));
-// }
-//
-// // TODO:
-// // While not DQ
-// // > Scan String
-// t_token	*token_scan_string_double(t_scanner *s)
-// {
-// 	char	c;
-//
-// 	while (scanner_peek(s) && scanner_peek(s) != '"')
-// 	{
-// 		s->start = s->current;
-// 		c = scanner_advance(s);
-// 		if (c == '$' && is_valid_ident(scanner_peek(s)))
-// 		{
-// 			if (!scanner_add_token(s, DOLLAR))
-// 				return (NULL);
-// 			s->start = s->current;
-// 			if (scanner_peek(s) != '$' && !token_scan_ident(s))
-// 				return (NULL);
-// 		}
-// 		else
-// 		{
-// 			if (!scan_part(s))
-// 				return (NULL);
-// 		}
-// 	}
-// 	if (scanner_peek(s) == '\0')
-// 	{
-// 		return (scanner_error('"'));
-// 	}
-// 	scanner_advance(s);
-// 	return ((t_token *)1);
-// }

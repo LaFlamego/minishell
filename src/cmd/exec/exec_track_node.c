3081b0560ccc7e757c18747909d6d93c86a08ecc
Oery <coincoin@baozi>
2026-05-11 13:35:47 +0200

    fix: last exit code not always updating

--- src/cmd/exec/exec_track_node.c
+++ src/cmd/exec/exec_track_node.c
@@ -6,12 +6,12 @@
 /*   By: crevette <coincoin@baozi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/19 20:24:51 by crevette          #+#    #+#             */
-/*   Updated: 2026/05/09 12:03:57 by crevette         ###   ########.fr       */
+/*   Updated: 2026/05/11 13:30:08 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "src/ctx/ctx.h"
 #include "exec.h"
+#include "src/ctx/ctx.h"
 #include <unistd.h>
 
 unsigned int	track_node(t_cmd_node *node, t_ctx *ctx)
@@ -26,15 +26,12 @@ unsigned int	track_node(t_cmd_node *node, t_ctx *ctx)
 		exec_ctx.is_pipe = true;
 		exit_code = handle_pipe_sign(node, &exec_ctx, ctx);
 	}
-	else
-	{
-		exec_ctx.is_pipe = false;
-		if (node->kind == COMMAND)
-			exit_code = handle_single_command(node, ctx, &exec_ctx);
-		if (node->kind == OP_AND)
-			exit_code = handle_and_sign(node, ctx);
-		if (node->kind == OP_OR)
-			exit_code = handle_or_sign(node, ctx);
-	}
+	if (node->kind == COMMAND)
+		exit_code = handle_single_command(node, ctx, &exec_ctx);
+	if (node->kind == OP_AND)
+		exit_code = handle_and_sign(node, ctx);
+	if (node->kind == OP_OR)
+		exit_code = handle_or_sign(node, ctx);
+	env_set_exit_code(exit_code, ctx->env);
 	return (exit_code);
 }

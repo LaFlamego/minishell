9c218bb4d16298e4cfbffaf4b55457edcdb9316f
Oery <coincoin@baozi>
2026-05-14 22:14:14 +0200

    norm: extract dollarsign scanning

--- src/scanner/token/token_scan_string.c
+++ src/scanner/token/token_scan_string.c
@@ -6,7 +6,7 @@
 /*   By: Oery <coincoin@baozi>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 15:39:47 by Oery              #+#    #+#             */
-/*   Updated: 2026/05/10 22:10:01 by Oery             ###   ########.fr       */
+/*   Updated: 2026/05/14 22:09:20 by Oery             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,6 @@ t_token	*token_scan_string(t_scanner *s)
 	return (scanner_add_token_lit(s, STRING, text));
 }
 
-// NOTE:
-// If string starts with a symbol or a number
-// It's length is 1
-// Else read until blank / symbol
-
-// FIXME: An empty string should not add any token
-
 static t_token	*scan_dollar(t_scanner *s)
 {
 	char	*text;
@@ -63,29 +56,39 @@ static t_token	*scan_dollar(t_scanner *s)
 	return (token_scan_ident(s));
 }
 
-t_token	*token_scan_string_double(t_scanner *s)
+static int	traverse_string(t_scanner *s, t_token **t)
 {
 	char	c;
-	char	*text;
-	t_token	*t;
 
-	t = NULL;
-	s->start = s->current;
 	c = scanner_peek(s);
 	while (c && c != '"')
 	{
 		if (scanner_match(s, '$'))
 		{
-			t = scan_dollar(s);
-			if (!t)
-				// FIXME: handle error
-				return (NULL);
+			*t = scan_dollar(s);
+			if (!*t)
+			{
+				scanner_error('$');
+				return (0);
+			}
 			s->start = s->current;
 		}
 		else
 			scanner_advance(s);
 		c = scanner_peek(s);
 	}
+	return (1);
+}
+
+t_token	*token_scan_string_double(t_scanner *s)
+{
+	char	*text;
+	t_token	*t;
+
+	t = NULL;
+	s->start = s->current;
+	if (!traverse_string(s, &t))
+		return (NULL);
 	if (!scanner_match(s, '"'))
 		return (scanner_error('"'));
 	if (s->current > s->start)
